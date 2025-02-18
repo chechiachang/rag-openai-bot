bc787c530f16b962d1d3c0736edfb22e9e50a65a
chechia <chechia@maicoin.com>
2025-02-18 23:34:25 +0800

    feat OB-9: change user prompt

--- src/bot/conversational_retrieval_agent.py
+++ src/bot/conversational_retrieval_agent.py
@@ -30,17 +30,21 @@ class ConversationalRetrievalAgent:
             res.append(f"Human:{human}\nAI:{ai}")
         return "\n".join(res)
 
-    # Method to set up the bot
+    # Method to set up the chain
     def setup_bot(self):
         embed_manager = EmbeddingManager()
         self.vectordb = embed_manager.vectordb
         # Create a retriever from the vector database
         retriever = self.vectordb.as_retriever(
-            search_type="similarity",
-            search_kwargs={"k": 6}
+            #search_type="similarity",
+            search_type="mmr",
+            search_kwargs={
+                "k": 4,
+                #"score_threshold": 0.5,
+            }
         )
         # Create a ConversationalRetrievalChain from the OpenAI model and the retriever
-        self.bot = ConversationalRetrievalChain.from_llm(
+        self.chain = ConversationalRetrievalChain.from_llm(
             self.llm,
             retriever,
             return_source_documents=True,
@@ -49,14 +53,25 @@ class ConversationalRetrievalAgent:
 
     def generate_prompt(self, question):
         if not self.chat_history:
-            # Se è la prima domanda, usa un template specifico senza contesto di conversazione precedente
-            prompt = f"""You are an assistant for question-answering tasks.
-            Use the following pieces of retrieved context to answer the question.
-            If you don't know the answer, just say that you don't know.
-            Question: {question}
-            Context: \n
-            Answer:
+            prompt = f"""你是一個問答任務的助手。根據 Retrival 的資料內容回答 Kubernetes (k8s) 相關問題
+            # 步驟
+            1. 根據內容準確回答問題，確保答案清晰且準確，避免加入任何虛構或未經證實的資訊。
+            2. 如果內容中沒有直接回答，請根據相關資訊進行合理推斷，但不要編造答案。
+            3. 使用符合台灣用語習慣的表達方式，提高可讀性。
+            4. 確保最終輸出內容為台灣繁體中文。如果是英文專有名詞，可以在字詞後加註原文。
+               例如：「人工智慧（Artificial Intelligence）」。
+            5. 如果內容有文章連結網址，補上網址與域名 https://kubernetes.io/ 並完整顯示。
+               例如文章如有 「/docs/concepts/overview」，則顯示完整網址為 「https://kubernetes.io/docs/concepts/overview」。如果沒有則不顯示。
+            6. 如果內容有文章 title，顯示原文 title 不需要翻譯。
+            問題：{question}
             """
+            #prompt = f"""You are an assistant for question-answering tasks.
+            #Use the following pieces of retrieved context to answer the question.
+            #If you don't know the answer, just say that you don't know.
+            #Question: {question}
+            #Context:
+            #Answer:
+            #"""
         else:
             # If it is the first question, use a specific template without previous conversation context
             print("Chat with previous history")
@@ -77,9 +92,9 @@ class ConversationalRetrievalAgent:
     @limits(calls=600, period=60)
     def ask_question(self, query):
         prompt = self.generate_prompt(query)
-        # Invoke the bot with the question and the chat history
-        result = self.bot.invoke({"question": prompt, "chat_history": self.chat_history})
-        # Append the question and the bot's answer to the chat history
+        # Invoke the chain with the question and the chat history
+        result = self.chain.invoke({"question": prompt, "chat_history": self.chat_history})
+        # Append the question and the chain's answer to the chat history
         self.chat_history.append((query, result["answer"]))
 
         # Return the bot's answer
