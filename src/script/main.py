a2acd943c68c9a86e9d8928eeb7141c16f5bd287
chechia <chechia@maicoin.com>
2025-03-15 17:38:03 +0800

    fix: quip client

--- src/script/main.py
+++ src/script/main.py
@@ -1,10 +1,9 @@
 import os
-import json
 
 from dotenv import find_dotenv
 from dotenv import load_dotenv
+from quipclient import QuipClient
 
-from .quip.client import QuipClient
 
 def main():
     load_dotenv(find_dotenv(raise_error_if_not_found=True, usecwd=True))
@@ -13,12 +12,12 @@ def main():
     )
     user = client.get_authenticated_user()
 
-    #starred = client.get_folder(user["starred_folder_id"])
-    #print(f"There are {len(starred['children'])} items in your starred folder")
+    starred = client.get_folder(user["starred_folder_id"])
+    print(f"There are {len(starred['children'])} items in your starred folder")
 
-    query = "SOP: RDS"
-    threads = client.get_matching_threads(query, count=None, only_match_titles=False)
-    print(f"There are {len(threads)} threads matching '{query}'")
-    for thread in threads:
-        #print(thread["thread"]["title"])
-        print(json.dumps(thread, indent=2))
+    #query = "SOP: RDS"
+    #threads = client.get_matching_threads(query, count=None, only_match_titles=False)
+    #print(f"There are {len(threads)} threads matching '{query}'")
+    #for thread in threads:
+    #    #print(thread["thread"]["title"])
+    #    print(json.dumps(thread, indent=2))
